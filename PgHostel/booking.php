c51e1858f6cf294b1bc1e2c14ddbe494e6f50043
annmaryajith <118360733+annmaryajith@users.noreply.github.com>
2023-11-30 23:34:11 +0530

    Add files via upload

--- PgHostel/booking.php
+++ PgHostel/booking.php
@@ -10,56 +10,38 @@ if (!isset($_SESSION['username'])) {
 // Include your database connection file
 include('conn.php');
 
+// Initialize a variable to store the alert message
+$alert_message = "";
+
 // Retrieve user details from the session
 $username = $_SESSION['username'];
 
 // Retrieve booking details from the form
-// $pg_name = mysqli_real_escape_string($conn, $_POST['pg_name']);
-// $room_type = mysqli_real_escape_string($conn, $_POST['room_type']);
-// $check_in = $_POST['check_in'];
-// $check_out = $_POST['check_out'];
+if (isset($_GET['pg_id'])) {
+    $pg_id = mysqli_real_escape_string($conn, $_GET['pg_id']);
 
-// Fetch the PG details from the database
-$pg_query = "SELECT pg_id, pg_name FROM pg WHERE pg_name = 'Your_Pg_Name'";
-$pg_result = $conn->query($pg_query);
-// Check if the PG is available
-if ($pg_result->num_rows == 1) {
-    $pg_data = $pg_result->fetch_assoc();
-    $selected_pg_id = $pg_data['pg_id'];
-} else {
-    // Handle the case when the PG is not found
-    echo "PG not found.";
-    exit();
-}
+    // Get user_id using the username
+    $user_query = "SELECT user_id FROM users WHERE username = '$username'";
+    $user_result = $conn->query($user_query);
 
+    if ($user_result->num_rows == 1) {
+        $user_data = $user_result->fetch_assoc();
+        $user_id = $user_data['user_id'];
 
-if (isset($_POST['check_in'])) {
-    $check_in = $_POST['check_in'];
+        // Insert the booking into the database
+        $insert_query = "INSERT INTO bookings (user_id, pg_id, booking_date, status) 
+                         VALUES ('$user_id', '$pg_id', NOW(), 'booked')";
 
-// Get user_id using the username
-$user_query = "SELECT user_id FROM users WHERE username = '$username'";
-$user_result = $conn->query($user_query);
-
-if ($user_result->num_rows == 1) {
-    $user_data = $user_result->fetch_assoc();
-    $user_id = $user_data['user_id'];
-
-    // Insert the booking into the database
-    $insert_query = "INSERT INTO bookings (user_id,  booking_date) 
-                     VALUES ('$user_id', '$check_in')";
-
-    if ($conn->query($insert_query) === TRUE) {
-        echo '<script>alert("Booking successful!");</script>';
-    } else {
-        echo "Error: " . $insert_query . "<br>" . $conn->error;
+        if ($conn->query($insert_query) === TRUE) {
+            // Set the alert message
+            $alert_message = "Booking successful!";
+        } else {
+            // Set the alert message with an error
+            $alert_message = "Error: " . $insert_query . "<br>" . $conn->error;
+        }
     }
 }
-}
-    // Redirect to a confirmation page
-    // header("Location: booking.php");
-    // exit();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -70,7 +52,17 @@ if ($user_result->num_rows == 1) {
 </head>
 <body>
     <h2>PG Booking Form</h2>
-    <form action="booking.php" method="post">   
+    <form action="" method="get" onsubmit="return showAlert()">  
+        <script>
+            function showAlert() {
+                // Display the alert message based on PHP logic
+                <?php echo "alert('$alert_message');" ?>
+                // Return false to prevent the form from submitting
+                return false;
+            }
+        </script>
+        
+        <!-- <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>"> -->
 
         <!-- <label for="room_type">Room Type:</label>
         <select id="room_type" name="room_type" required>
@@ -79,12 +71,12 @@ if ($user_result->num_rows == 1) {
             <option value="shared">Shared Room</option>
         </select> -->
 
-        <input type="hidden" name="selected_pg" value="<?php echo $selected_pg_id; ?>">
+        <input type="hidden" name="pg_id" value="<?php echo $row['pg_id']; ?>">
+
         <label for="check_in">Booking Date:</label>
         <input type="date" id="check_in" name="check_in" value="<?php echo date('Y-m-d'); ?>" required>
         
-        <input type="submit" value="Book Now">
+        <input type="submit" value="Pay Now">
     </form>
 </body>
 </html>
-
