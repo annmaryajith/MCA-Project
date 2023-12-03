ceab6f9fa044da2c5e64e1c5f0534727e1361ea8
annmaryajith <118360733+annmaryajith@users.noreply.github.com>
2023-12-03 10:38:59 +0530

    Add files via upload

--- PgHostel/viewpgroom.php
+++ PgHostel/viewpgroom.php
@@ -53,7 +53,7 @@
         } */
 
         table {
-            width: 100%;
+            width: 120%;
             border-collapse: collapse;
             margin-top: 20px;
         }
@@ -83,88 +83,64 @@
                 <li><a href="logout.php" class="logout">Logout</a></li>
             </ul>
         </div>
-        <!-- <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body {
-            font-family: Arial, sans-serif;
-        }
-
-        .containerr {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        /* .logout {
-            float: right;
-        } */
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-
-        th, td {
-            border: 1px solid #ddd;
-            padding: 10px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-        </style> -->
     </head>
     <body>
     <div class="containerr">
     <h2>Rooms</h2>
     <?php
-    include('conn.php');
-    $sql = "SELECT room_number, room_type, price FROM pg_room";
-    $result = $conn->query($sql);
-    
-    if ($result->num_rows > 0) {
-        echo '<table>
-                <tr>
-                    <th>Room number</th>
-                    <th>Room type</th>
-                    <th>Price</th>
-                    <th>Action</th>
-                </tr>';
-    
-        while ($row = $result->fetch_assoc()) {
-            $room_number = $row["room_number"];
-            $room_type = $row["room_type"];
-            echo "<tr>
-                    <td>" . $room_number . "</td>
-                    <td>" . $room_type . "</td>
-                    <td>" . $row["price"] . "</td>
-                    <td>
-                <button onclick=\"updateRoom('" . $room_number . "')\">Update</button>
-            </td>";
-            // if ($status == 1) {
-            //     echo "<button onclick=\"changeStatus('$username', 0);\">Deactivate</button>";
-            // } else {
-            //     echo "<button onclick=\"changeStatus('$username', 1);\">Activate</button>";
-            // }
-            echo "</td>
-                </tr>";
+session_start();
+include('conn.php');
+
+// Check if the owner is logged in
+if (isset($_SESSION['username'])) {
+    $owner_username = $_SESSION['username'];
+
+    // Get the owner's ID from the owners table based on the username
+    $owner_query = "SELECT pgowner_id FROM pg_owners WHERE username = '$owner_username'";
+    $owner_result = $conn->query($owner_query);
+
+    if ($owner_result->num_rows > 0) {
+        $owner_row = $owner_result->fetch_assoc();
+        $owner_id = $owner_row['pgowner_id'];
+
+        // Retrieve rooms associated with the owner
+        $room_query = "SELECT room_number, room_type, price FROM pg_room WHERE pgowner_id = $owner_id";
+        $room_result = $conn->query($room_query);
+
+        if ($room_result->num_rows > 0) {
+            echo '<table>
+                    <tr>
+                        <th>Room number</th>
+                        <th>Room type</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>';
+
+            while ($row = $room_result->fetch_assoc()) {
+                $room_number = $row["room_number"];
+                $room_type = $row["room_type"];
+                echo "<tr>
+                        <td>" . $room_number . "</td>
+                        <td>" . $room_type . "</td>
+                        <td>" . $row["price"] . "</td>
+                        <td>
+                            <button onclick=\"updateRoom('" . $room_number . "')\">Update</button>
+                        </td>
+                    </tr>";
+            }
+            echo '</table>';
+        } else {
+            echo "No rooms found for the logged-in owner.";
         }
-        echo '</table>';
     } else {
-        echo "No users found.";
+        echo "Owner not found.";
     }
-    $conn->close();
-    ?>
+} else {
+    echo "Owner not logged in.";
+}
+
+$conn->close();
+?>
     </div>
     <script>
     function updateRoom(roomNumber) {
